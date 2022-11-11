5e9fac7659b1e8e5cfd74f96725cc980f39f10fa
mbende sampson <mbende2000@yahoo.com>
2022-11-11 23:18:55 +0100

    cloudinary

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["assets.stickpng.com"],
+    domains: ["assets.stickpng.com", "unsplash.com", "res.cloudinary.com"],
   },
 };
 
